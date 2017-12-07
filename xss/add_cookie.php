555ccf8e74ee07e63b8c2166f87fe26ab4a5d9da
Jan Jaap Siewers <janjaap@roc-dev.com>
2017-12-07 14:28:17 +0100

    xss scripting toegevoegd met ajax request

--- xss/add_cookie.php
+++ xss/add_cookie.php
@@ -5,6 +5,13 @@
  * Date: 07-12-17
  * Time: 13:18
  */
+?>
+
+<script>
+console.log(<?php print_r($_POST); ?>);
+</script>
+
+<?php
 extract($_POST);
 $link = mysqli_connect("localhost", "pipo", "declown", "vulnerable_db");
 $query = "INSERT into cookie (cookievalue) VALUES ('".$cookie."')";
