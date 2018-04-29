5e020806b5ebf293d52dd7c595881536232f86e5
Jan Jaap Siewers <janjaap@roc-dev.com>
2018-04-29 11:53:30 +0200

    File uploads aangepast

--- direct_obj_reference/upload.php
+++ direct_obj_reference/upload.php
@@ -10,59 +10,55 @@ echo "<p>direct object reference</p>";
 echo "<h1>File upload</h1>";
 include("nav.php");
 
-$target_dir = "uploads/";
+$target_dir = "/var/uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
+$uploadOk = 0;
 
-/*
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".<br>";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.<br>";
+
+    // Allow certain file formats
+    if( $imageFileType != "jpg"
+        && $imageFileType != "png"
+        && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
         $uploadOk = 0;
     }
-}
-*/
 
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.<br>";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.<br>";
-    $uploadOk = 0;
-}
-// Allow certain file formats
+    // Check if image file is a actual image or fake image
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".<br>";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.<br>";
+            $uploadOk = 0;
+        }
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.<br>";
+        $uploadOk = 0;
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.<br>";
+        $uploadOk = 0;
+    }
 
-/*
-if( $imageFileType != "jpg"
-    && $imageFileType != "png"
-    && $imageFileType != "php" //Voeg php toe om ook scriptfiles toe te staat....levensgevaaaarlijk!!!
-    && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
-    $uploadOk = 0;
-}
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
 
-*/
-
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
+    // if everything is ok, try to upload file
     } else {
-        echo "Sorry, there was an error uploading your file.<br>";
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
+        } else {
+            echo "Sorry, there was an error uploading your file.<br>";
+        }
+
     }
 }
 
