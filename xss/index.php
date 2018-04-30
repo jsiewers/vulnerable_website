babfe39c10fffa63d39504ceca3362723d3e9cca
Jan Jaap Siewers <janjaap@roc-dev.com>
2018-04-30 09:41:41 +0200

    session destroy

--- xss/index.php
+++ xss/index.php
@@ -9,7 +9,7 @@
 // Initialize the session.
 // If you are using session_name("something"), don't forget it now!
 session_start();
-/*
+
 // Unset all of the session variables.
 $_SESSION = array();
 
@@ -25,7 +25,7 @@ if (ini_get("session.use_cookies")) {
 
 // Finally, destroy the session.
 session_destroy();
-*/
+
 
 
 /*
